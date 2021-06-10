e6208ef52194863f39ee2adef45b9d16d99721cf
mathemartins1 <mathegeniuse@gmail.com>
2021-06-10 16:36:48 -0700

    ok

--- vendescrow/blockchain/utils.py
+++ vendescrow/blockchain/utils.py
@@ -79,7 +79,9 @@ def transfer_crypto_with_sender_address(crypto_network_api: str, sender_address:
     thisCheck = block_io.create_and_sign_transaction(prepare_data=prepare_trx)
     print(thisCheck)
     submit_url = "https://block.io/api/v2/submit_transaction/?api_key={apiKey}"
-    return requests.post(submit_url, thisCheck, proxies=proxies)
+    trx_hash = requests.post(submit_url, thisCheck, proxies=proxies)
+    print(trx_hash)
+    return trx_hash
 
 
 # Transfer Crypto Asset
